664ea986b29bb30e085bf5f18350ab10b7f4571c
Rácz Igor <raczigor26@gmail.com>
2025-01-10 18:44:57 +0100

    added js cc

---
+++ JS Crash Course/main.js
@@ -0,0 +1,21 @@
+//var, let, const
+
+const name = 'John';
+const age = 20;
+/*
+//Concatenation
+console.log('My name is ' + name + ' and I am ' + age);
+//Template String
+console.log(`My name is ${name} and I am ${age}`);
+
+*/
+
+//Arrays
+
+const fruits = ['apples', 'oranges', 43];
+
+fruits.push('mangos'); //add to last place
+fruits.unshift('banana'); // add to first place
+fruits.pop(); // remove last
+Array.isArray(fruits); //check if its array
+fruits.indexOf('banana') // check what is the index of banana
