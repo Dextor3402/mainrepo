886ca56d9a81b3a209744a49bd5b5f380c0b102d
Rácz Igor <raczigor26@gmail.com>
2025-01-25 19:06:25 +0100

    comments, shortcuts

---
+++ Jacascript Sandbox/01-variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,21 @@
+// THis is a single line of code
+//Multi line comment: Shift + Alt + A
+
+/* ==
+fdgdfg
+
+dfgzh
+sdcvxcv
+
+ */
+
+//Shift + up/down: highlight multiple line of code || left/right
+//aLT+UP=DOWN move a line of code
+//CTRL+D select next instance of highlighted element
+//ALt+click you can write veerywhere u clicked at once
+
+/* 
+console.log('hefddllo world');
+console.log('hello wofddrld');
+console.log('hfddello world'); 
+*/
