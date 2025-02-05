a802edbd6a90e29f76ecdb458bd854cc06eb01bc
Rácz Igor <raczigor26@gmail.com>
2025-02-05 18:42:57 +0100

    numbers

---
+++ Jacascript Sandbox/01-variables-data-types/11-numbers/script.js
@@ -0,0 +1,28 @@
+let x;
+
+
+const num = new Number (5.5322);
+
+x = num.toString();
+
+x = num.toString().length;
+
+x = num.toFixed(2);
+
+x = num.toPrecision(2);
+
+x = num.toExponential(2);
+
+x = num.toLocaleString('en-US');
+
+x = num.valueOf();
+
+x = Number.MAX_VALUE;
+
+X = Number.MIN_VALUE;
+
+
+
+
+
+console.log(x);
