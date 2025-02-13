871ff184dc516e74db88ca6cd76251db6a3c24f6
Rácz Igor <raczigor26@gmail.com>
2025-02-13 20:24:34 +0100

    arrays

---
+++ Jacascript Sandbox/02-arrays-and-objects/01-array-basics/script.js
@@ -0,0 +1,31 @@
+let x;
+
+//Array COnstructor
+
+const numbers = [12,43,12,63,16];
+const mixed = [12,'hey', true, null]
+
+//array constructor
+
+const fruits = new Array('apple', 'grape', 'orange');
+
+
+
+
+x = numbers[0];
+
+x = numbers[0] + numbers[2];
+
+x = numbers.length;
+
+fruits[2] = 'pear';
+
+x = fruits;
+
+fruits[3] = 'strawberry';
+
+fruits[fruits.length] = 'blueberry';
+fruits[fruits.length] = 'peach';
+
+
+console.log(x);
---
+++ Jacascript Sandbox/02-arrays-and-objects/02-array-methods/script.js
@@ -0,0 +1,3 @@
+const arr = [1,2,5,87,23,65]
+
+console.log(arr);
