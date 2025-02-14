cbf23c4647fe6ddaaabf7328ee58737f40b5185a
Rácz Igor <raczigor26@gmail.com>
2025-02-14 10:15:39 +0100

    array challenge

--- Jacascript Sandbox/02-arrays-and-objects/02-array-methods/script.js
+++ Jacascript Sandbox/02-arrays-and-objects/02-array-methods/script.js
@@ -1,3 +1,32 @@
-const arr = [1,2,5,87,23,65]
+let x;
 
-console.log(arr);
+const arr = [1,2,5,87,23,65]
+/*
+arr.push(100);
+
+arr.pop();
+
+arr.unshift(99);
+
+arr.shift();
+
+arr.reverse();
+*/
+x = arr.includes(20);
+
+x = arr.indexOf(5);
+
+x = arr.slice(1, 4);
+
+//x = arr.splice(1, 4);
+
+//x = arr.splice(3,1)
+
+x = arr.splice(1, 4).reverse().toString().charAt(0);
+
+
+
+
+
+
+console.log(x);
---
+++ Jacascript Sandbox/02-arrays-and-objects/04-array-challenge/script.js
@@ -0,0 +1,18 @@
+/*
+const arr = [1, 2, 3, 4, 5];
+
+
+arr.unshift(0);
+arr.push(6);
+arr.reverse(arr).toString();
+
+console.log(arr);
+*/
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+
+arr1.pop();
+const arr3 = arr1.concat(arr2);
+
+console.log(arr3);
