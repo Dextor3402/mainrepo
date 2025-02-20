1eb0afedf109b4eae23c0d533693662ae59e6bbc
Rácz Igor <raczigor26@gmail.com>
2025-02-20 18:12:37 +0100

    func basic

---
+++ Jacascript Sandbox/03-functions-scope/01-function-basics/script.js
@@ -0,0 +1,22 @@
+function sayHello() {
+    console.log('Hello world');
+}
+
+sayHello();
+
+function add(num1,num2){
+    console.log(num1+num2);
+}
+
+add(1,1);
+
+function subtract(num1,num2){
+    return num1-num2;
+}
+
+console.log(subtract(10,5));
+
+
+const result = subtract(10,5);
+
+console.log(result);
