a12203a4fb75f5f4da20e09f35f818c2d1a7e24b
Rácz Igor <raczigor26@gmail.com>
2025-03-03 05:37:34 +0100

    if state

---
+++ Jacascript Sandbox/04-logic-control-flow/01-if-statements/script.js
@@ -0,0 +1,29 @@
+if(true){
+    console.log('true');
+}
+
+if(false){
+    console.log('false');
+}
+
+const x = 1;
+const y = 10;
+
+if(x>=y) {
+    console.log('x is greater or equal than y');
+}
+
+if(x === y) {
+    console.log('x is equal than y');
+}else{
+    console.log('x is not equal to y');
+}
+
+if(x != y){
+    const z = 30;
+    console.log('z is 30');
+}
+
+//shorthand if
+if(x>=y) console.log('x is greater or equal than y');
+else console.log('this is false');
